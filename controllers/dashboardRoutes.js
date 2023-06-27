ea99dc444634e0b7e3180c01045d4f23fee54bc4
Diego Borjas <diegoborjas@gmail.com>
2023-06-27 18:08:07 -0500

    adjusted routes, added Comment into models

---
+++ controllers/dashboardRoutes.js
@@ -0,0 +1,55 @@
+const router = require('express').Router();
+const { Post, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+// Get all posts to show on Dashboard
+router.get('/', withAuth, async (req, res) => {
+    try {
+      const postData = await Post.findAll({
+         where: {
+            user_id: req.session.user_id
+        },
+        include: [
+          {
+            model: User,
+            attributes: ['name'],
+          },
+        ],
+        logging: console.log
+      });
+  
+      // Serialize data so the template can read it
+      const posts = postData.map((post) => post.get({ plain: true }));
+  
+      // Pass serialized data and session flag into template
+      res.render('dashboard', { 
+        posts, 
+        logged_in: req.session.logged_in 
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+  router.get('/dashboard', withAuth, async (req, res) => {
+    try {
+      // Find the logged in user based on the session ID
+      const userData = await User.findByPk(req.session.user_id, {
+        attributes: { exclude: ['password'] },
+        include: [{ model: Post }],
+      });
+  
+      const user = userData.get({ plain: true });
+  
+      res.render('dashboard', {
+        ...user,
+        logged_in: true
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+
+
+  module.exports = router;
